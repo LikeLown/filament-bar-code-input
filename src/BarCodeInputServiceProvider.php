ac51908a2b36bc264c55fb5f76782df267f2f8e7
LikeLown <109971166+LikeLown@users.noreply.github.com>
2025-02-26 07:32:04 +0000

    Fix styling

--- src/BarCodeInputServiceProvider.php
+++ src/BarCodeInputServiceProvider.php
@@ -58,9 +58,7 @@ class BarCodeInputServiceProvider extends PackageServiceProvider
         }
     }
 
-    public function packageRegistered(): void
-    {
-    }
+    public function packageRegistered(): void {}
 
     public function packageBooted(): void
     {
